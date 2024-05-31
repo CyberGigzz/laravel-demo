884afd1eaf6ae20d4b7e533c4177481587b47627
Giga <gigakatamadze0@gmail.com>
2024-05-31 12:15:05 +0400

    Created model

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,30 +1,8 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-class Job {
-    public static function all(): array {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Web programmer',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Frontend',
-                'salary' => '$80,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Backend',
-                'salary' => '$100,000'
-            ]
-            
-        ]; 
-    }
-}
+use App\Models\Job;
 
 
 
@@ -42,7 +20,7 @@ Route::get('/jobs', function ()  {
 
 Route::get('/jobs/{id}', function ($id)  {
 
-    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
