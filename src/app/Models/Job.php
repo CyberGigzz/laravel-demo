37835467d4a33b422135d61e88078535c6025e05
Giga <gigakatamadze0@gmail.com>
2024-05-31 13:58:38 +0400

    Ran some migrations

--- src/app/Models/Job.php
+++ src/app/Models/Job.php
@@ -2,36 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Web programmer',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Frontend',
-                'salary' => '$80,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Backend',
-                'salary' => '$100,000'
-            ]
-            
-        ]; 
-    }
-
-    public static function find(int $id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-        
-        if (! $job) {
-            abort(404);
-        }
-        return $job;
-    } 
+class Job extends Model {
+    
 }
