415ec00a0f2a0844589b0e08ff9a44122a2a6a6d
Giga <gigakatamadze0@gmail.com>
2024-06-07 15:04:10 +0400

    Working on register and sign in. implemented some controllers and authentication

---
+++ src/app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Validation\Rules\Password;
+
+class RegisteredUserController extends Controller
+{
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(6), 'confirmed'],
+    
+        ]);
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
-
-
 
 Route::view('/', 'home');
 
@@ -19,3 +19,9 @@ Route::resource('jobs', JobController::class);
 //     Route::patch('/jobs/{job}',  'update');
 //     Route::delete('/jobs/{job}',  'destroy');
 // });
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
