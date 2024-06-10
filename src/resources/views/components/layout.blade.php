7b5ce7f9dae58cfe2be5748360c19bd5d885e31f
Giga <gigakatamadze0@gmail.com>
2024-06-10 10:25:15 +0400

    For now i have finished working on project, may continue in future

--- src/resources/views/components/layout.blade.php
+++ src/resources/views/components/layout.blade.php
@@ -4,8 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>My Website</title>
 </head>
 
--- src/vite.config.js
+++ src/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8000', // Assuming your Laravel backend is running on port 8000
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
-    },
+    // server: {
+    //     proxy: {
+    //         '/api': {
+    //             target: 'http://localhost:8000', // Assuming your Laravel backend is running on port 8000
+    //             changeOrigin: true,
+    //             rewrite: (path) => path.replace(/^\/api/, ''),
+    //         },
+    //     },
+    // },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
